08cb06ef6c4d9fb1bb653d1d25966f9b65f20a0c
Isaac Evans <ine@mit.edu>
2011-09-10 21:47:42 -0400

    Events widget added

--- views.py
+++ views.py
@@ -4,5 +4,18 @@ from django.shortcuts import render_to_response
 #def home(request):
 #    return HttpResponse("Hello, world. You're at the poll index.")
 
-def home(req):
-    return render_to_response('index.html', { 'date': '1-2-2011' })
+#def home(req):
+#    return render_to_response('index.html', { 'date': '1-2-2011' })
+
+def home(request):
+    import feedparser
+    url = "http://events.mit.edu/rss/index.html?fulltext=&andor=and&categories=24&categories=4&categories=127&categories=2&categories=7&sponsors%3A0=&_rss=Create+RSS+Feed"
+    d2 = feedparser.parse(url)
+    smallevents = [] # contains only the things we want
+    for event in d2['entries']:
+        smallevents.extend([ { 'title' : event['title'],
+                               'date' : event['date'],
+                               'description' : event['description'] } ])
+
+    print smallevents
+    return render_to_response('events.html', { 'events' : smallevents })
